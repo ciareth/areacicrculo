12bf5b42731c4b9309568bc2361327391656a3f9
damian <jiselsantos18@gmail.com>
2024-09-18 14:34:57 -0600

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,5 @@
+// Caclcular el area de un circulo. Math.PI, Math.Round()
+let r = prompt("Ingrese el valor del radio del Circulo");
+area = Math.PI*(r*r);
+
+document.write("El area del circulo con radio de ", r, " es igual a: ", area);
